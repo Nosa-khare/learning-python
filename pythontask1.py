1cf619485311e2c53e85224191734428d99aa489
Nosakhare Edokpayi <59267671+Nosa-khare@users.noreply.github.com>
2020-03-27 17:18:09 +0100

    Update pythontask1.py

--- pythontask1.py
+++ pythontask1.py
@@ -2,6 +2,7 @@ print("Calculate the Area of a Circle!\n")
 
 pi = 22/7
 r = radius = int(input("Type in the radius: "))
-area = pie * r * r
+r_squared = r * r
+area = pi * r_squared
 
 print("\nArea of circle = " + str(area))
