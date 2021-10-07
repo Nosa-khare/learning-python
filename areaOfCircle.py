59506b4ec3ce90cdf9e4b4de276745453016c4ec
Nosakhare Edokpayi <co.nosakhare@gmail.com>
2021-10-07 16:09:16 +0100

    deleted area of cirle

--- areaOfCircle.py
+++
@@ -1,8 +0,0 @@
-print("Calculate the Area of a Circle!\n")
-
-pi = 22/7
-r = radius = int(input("Type in the radius: "))
-r_squared = r * r
-area = pi * r_squared
-
-print("\nArea of circle = " + str(area))
